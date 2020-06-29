d50af2d2f6a23aa5bebff063292209434e9442eb
oguzhan.ozturk <oguzhan_687@hotmail.com>
2020-06-29 09:59:44 +0300

    re

--- fihrist/csvim.py
+++ fihrist/csvim.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 from fihrist.models import Personel
 
-pathd=Path("C:\\Users\\atessu\\Desktop\\xx\\calisan_data.csv")
+pathd=Path("C:\\Users\\oguzhan.ozturk\\Desktop\\xx\\calisan_data.csv")
 data=pd.read_csv(pathd)
 data2=pd.DataFrame(data,columns =['Isimler', 'Soyisimler','TelefonTuru','Departman','Sehir','DogumTarihi','Maas','Telefon','mail'])
-personel=Personel.objects.get_or_create(isim=[""])
+personel=Personel.objects.all()
 print(data2)
