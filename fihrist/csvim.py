f96ae4d99f4968402b1254a223b25dac1f9dbf3a
oguz687 <oguzhan_687@hotmail.com>
2020-06-29 09:58:35 +0300

    son hali

--- fihrist/csvim.py
+++ fihrist/csvim.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import pandas as pd
 
-from fihrist.models import Personel
+# from fihrist.models import Personel
 
 pathd=Path("C:\\Users\\atessu\\Desktop\\xx\\calisan_data.csv")
 data=pd.read_csv(pathd)
 data2=pd.DataFrame(data,columns =['Isimler', 'Soyisimler','TelefonTuru','Departman','Sehir','DogumTarihi','Maas','Telefon','mail'])
-personel=Personel.objects.get_or_create(isim=[""])
+# personel=Personel.objects.all()
 print(data2)
