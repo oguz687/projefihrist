66e279ab5d656718fa0e5a586988777232edcea1
oguzhan.ozturk <oguzhan_687@hotmail.com>
2020-06-29 17:01:00 +0300

    re

--- fihrist/csvim.py
+++ fihrist/csvim.py
@@ -1,10 +1,19 @@
 from pathlib import Path
 import pandas as pd
 
-# from fihrist.models import Personel
+def ekle():
+    from pathlib import Path
+    import pandas as pd
 
-pathd=Path("C:\\Users\\atessu\\Desktop\\xx\\calisan_data.csv")
-data=pd.read_csv(pathd)
-data2=pd.DataFrame(data,columns =['Isimler', 'Soyisimler','TelefonTuru','Departman','Sehir','DogumTarihi','Maas','Telefon','mail'])
-# personel=Personel.objects.all()
-print(data2)
+    from fihrist.models import Personel
+
+    pathd = Path("C:\\Users\\oguzhan.ozturk\\Desktop\\xx\\calisan_data.csv")
+    data = pd.read_csv(pathd)
+    data2 = pd.DataFrame(data,columns=['Isimler', 'Soyisimler', 'TelefonTuru', 'Departman', 'Sehir', 'DogumTarihi', 'Maas','Telefon', 'mail'])
+    personel=Personel()
+    personel.objects.get_or_create(isim=data2["Isimler"],amirlik_yetki=0,amirlik=data2["Maas"],mudurluk_yetki=False,mudurluk=data2["Sehir"]
+                                   ,tc=data2["Telefon"],telefon=data2["Telefon"],adres=data2["Departman"],sicil=data2["Telefon"])
+
+
+if __name__ == "__main__":
+    ekle()
--- fihrist/models.py
+++ fihrist/models.py
@@ -62,3 +62,17 @@ class Personel(models.Model):
 
     def __str__(self):
         return self.isim
+
+
+def ekle():
+    from pathlib import Path
+    import pandas as pd
+
+    # from fihrist.models import Personel
+
+    pathd = Path("C:\\Users\\oguzhan.ozturk\\Desktop\\xx\\calisan_data.csv")
+    data = pd.read_csv(pathd)
+    data2 = pd.DataFrame(data,columns=['Isimler', 'Soyisimler', 'TelefonTuru', 'Departman', 'Sehir', 'DogumTarihi', 'Maas','Telefon', 'mail'])
+    personel=Personel()
+    personel.objects.get_or_create(isim=data2["Isimler"],amirlik_yetki=0,amirlik=data2["Maas"],mudurluk_yetki=False,mudurluk=data2["Sehir"]
+                                   ,tc=data2["Telefon"],telefon=data2["Telefon"],adres=data2["Departman"],sicil=data2["Telefon"])
